b734073e8f357cfe6177f7aac9eec69461fd3e53
shhridoy <shhridoy.cste@gmail.com>
2018-04-30 02:46:47 +0600

    version upgraded to 3 (Version name 1.0.2)

---
+++ app/src/main/java/com/cste10nstu/shhridoy/cste10nstu/Receiver/AlertReceiver.java
@@ -0,0 +1,16 @@
+package com.cste10nstu.shhridoy.cste10nstu.Receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by Dream Land on 10/27/2017.
+ */
+
+public class AlertReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
